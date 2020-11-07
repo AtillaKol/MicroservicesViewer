cc16bf740eae9bfe8a8de46e446aac466ba4e132
Atilla Kolali <atillakolali22@gmail.com>
2020-11-07 17:46:38 +0100

    Added configuration for the properties

---
+++ MicroservicesViewerBackend/src/main/java/ch/microserviceviewerbackend/configuration/MicroservicesPropertiesConfigurations.java
@@ -0,0 +1,30 @@
+package ch.microserviceviewerbackend.configuration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@ConfigurationProperties(value = "microservices")
+public class MicroservicesPropertiesConfigurations {
+
+    private static final Logger LOGGER = LogManager.getLogger(MicroservicesPropertiesConfigurations.class);
+
+    private List<String> uris;
+
+    public List<String> getUris() {
+        return uris;
+    }
+
+    public void setUris(List<String> uris) {
+        this.uris = uris;
+    }
+
+    @PostConstruct
+    public void init() {
+        if(uris == null) LOGGER.info("In the environment {} yml file, there are no uris defined.", System.getProperty("spring.profiles.active"));
+    }
+
+}
