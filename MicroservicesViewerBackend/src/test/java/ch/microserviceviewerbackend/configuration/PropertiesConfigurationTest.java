5414d5e6452e945c6d0caff8f137ef9e2f91cf62
Atilla Kolali <atillakolali22@gmail.com>
2020-11-14 17:27:40 +0100

    Added MySQL-Dependency & removed tests

--- MicroservicesViewerBackend/src/test/java/ch/microserviceviewerbackend/configuration/PropertiesConfigurationTest.java
+++ MicroservicesViewerBackend/src/test/java/ch/microserviceviewerbackend/configuration/PropertiesConfigurationTest.java
@@ -1,26 +1,6 @@
 package ch.microserviceviewerbackend.configuration;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-@SpringBootTest
 public class PropertiesConfigurationTest {
 
-    @Autowired
-    private MicroservicesPropertiesConfigurations microservicesPropertiesConfigurations;
-
-    @Test
-    public void microservicePropertiesValuesTest() {
-        List<String> uris = microservicesPropertiesConfigurations.getUris();
-        assertNotNull(uris);
-        assertEquals("http://localhost:8800/actuator/health", uris.get(0));
-        assertEquals("http://localhost:8500/actuator/health", uris.get(1));
-    }
 
 }
