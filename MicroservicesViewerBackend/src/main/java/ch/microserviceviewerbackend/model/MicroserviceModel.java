9ddc67adfe74f406d8a92a63d63acbb8c3b24691
Atilla Kolali <atillakolali22@gmail.com>
2020-11-15 21:13:59 +0100

    Added ViewerModel and integrated JSONProperty

--- MicroservicesViewerBackend/src/main/java/ch/microserviceviewerbackend/model/MicroserviceModel.java
+++ MicroservicesViewerBackend/src/main/java/ch/microserviceviewerbackend/model/MicroserviceModel.java
@@ -1,10 +1,19 @@
 package ch.microserviceviewerbackend.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({
+        "MicroserviceURI",
+        "ViewerUsername"
+})
 public class MicroserviceModel {
 
     //The ID is not needed to be inputted, because this will be auto generated.
 
+    @JsonProperty(value = "MicroserviceURI")
     private String microserviceURI;
+    @JsonProperty(value = "ViewerUsername")
     private String viewerUsername;
 
     public String getMicroserviceURI() {
