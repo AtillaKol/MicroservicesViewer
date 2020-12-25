0d4bbc107b4088f27e36c10df002e87ab203a976
Atilla Kolali <atillakolali22@gmail.com>
2020-12-25 15:51:34 +0100

    Backend geloescht

--- MicroservicesViewerBackend/src/main/java/ch/microserviceviewerbackend/dao/repository/MicroserviceRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.microserviceviewerbackend.dao.repository;
-
-import ch.microserviceviewerbackend.dao.daomodels.MicroserviceModelDAO;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MicroserviceRepository extends JpaRepository<MicroserviceModelDAO, Integer> {
-}
--- MicroservicesViewerBackend/src/main/java/ch/microserviceviewerbackend/dao/repository/ViewerRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.microserviceviewerbackend.dao.repository;
-
-import ch.microserviceviewerbackend.dao.daomodels.ViewerModelDAO;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ViewerRepository extends JpaRepository<ViewerModelDAO, String> {
-}
